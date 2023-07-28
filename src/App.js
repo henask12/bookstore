acb792edbb4a6a77169b786f33eae0a63b7d5b42
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-07-28 14:29:19 +0300

    made some tailwind css applied

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navigation from './components/Navigation';
 const App = () => (
   <div className="bg-gray-100 border border-gray-300 rounded-lg shadow-sm">
     <div className="p-8">
-      <div className="bg-white border border-white-100 rounded-lg shadow-sm">
+      <div className="bg-white border border-white-100 rounded-lg shadow-sm ml-8 mr-8">
         <Navigation />
       </div>
     </div>
